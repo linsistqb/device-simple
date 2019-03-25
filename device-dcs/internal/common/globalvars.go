ef92459074f88882ba73aae318773a2b5203798e
qb <745767382@qq.com>
2019-03-25 03:04:46 -0700

    20190325-1804

---
+++ device-dcs/internal/common/globalvars.go
@@ -0,0 +1,32 @@
+// -*- mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2018 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package common
+
+import (
+	ds_models "github.com/edgexfoundry/device-dcs/pkg/models"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/coredata"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logging"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/metadata"
+	"github.com/edgexfoundry/go-mod-core-contracts/models"
+)
+
+var (
+	ServiceName           string
+	ServiceVersion        string
+	CurrentConfig         *Config
+	CurrentDeviceService  models.DeviceService
+	UseRegistry           bool
+	ServiceLocked         bool
+	Driver                ds_models.ProtocolDriver
+	EventClient           coredata.EventClient
+	AddressableClient     metadata.AddressableClient
+	DeviceClient          metadata.DeviceClient
+	DeviceServiceClient   metadata.DeviceServiceClient
+	DeviceProfileClient   metadata.DeviceProfileClient
+	LoggingClient         logger.LoggingClient
+	ValueDescriptorClient coredata.ValueDescriptorClient
+)
